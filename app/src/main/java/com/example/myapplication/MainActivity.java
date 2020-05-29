53bfa08fcbc7bdd91d3b12d0da6c42a992d0b4bf
Andriy <ak8022@bs.amalnet.k12.il>
2020-05-29 19:55:20 +0300

    Linking components

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Switch mySwitch;
+    RadioButton rbr, rby, rbg, rbb;
+    Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mySwitch=(Switch)findViewById(R.id.mySwitch);
+        rbr=(RadioButton)findViewById(R.id.rbR);
+        rby=(RadioButton)findViewById(R.id.rbY);
+        rbg=(RadioButton)findViewById(R.id.rbG);
+        rbb=(RadioButton)findViewById(R.id.rbB);
+        button=(Button)findViewById(R.id.button);
     }
 }
