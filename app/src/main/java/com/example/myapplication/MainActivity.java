6a2c4850b00c52a5e0edebe3cd41d3af584ea76f
Andriy <ak8022@bs.amalnet.k12.il>
2020-05-29 19:57:55 +0300

    Swicth on click

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.Button;
@@ -12,17 +13,26 @@ public class MainActivity extends AppCompatActivity
     RadioButton rbr, rby, rbg, rbb;
     Button button;
 
+    boolean switchState;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mySwitch=(Switch)findViewById(R.id.mySwitch);
-        rbr=(RadioButton)findViewById(R.id.rbR);
-        rby=(RadioButton)findViewById(R.id.rbY);
-        rbg=(RadioButton)findViewById(R.id.rbG);
-        rbb=(RadioButton)findViewById(R.id.rbB);
-        button=(Button)findViewById(R.id.button);
+        mySwitch = (Switch) findViewById(R.id.mySwitch);
+        rbr = (RadioButton) findViewById(R.id.rbR);
+        rby = (RadioButton) findViewById(R.id.rbY);
+        rbg = (RadioButton) findViewById(R.id.rbG);
+        rbb = (RadioButton) findViewById(R.id.rbB);
+        button = (Button) findViewById(R.id.button);
+
+        switchState = mySwitch.isChecked();
+    }
+
+    public void switchClicked(View view)
+    {
+        switchState = mySwitch.isChecked();
     }
 }
