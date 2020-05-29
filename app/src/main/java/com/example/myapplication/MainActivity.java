a21c369fafdd07ee3d0ba8968ae8c54ad3e03a4a
Andriy <ak8022@bs.amalnet.k12.il>
2020-05-29 21:39:07 +0300

    Final push for today

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
     Switch mySwitch;
-    RadioButton rbr, rby, rbg, rbb;
     Button button;
+    LinearLayout layout;
+    RadioGroup rg;
+    RadioButton rbR, rbY, rbG, rbB;
 
     boolean switchState;
 
@@ -22,11 +29,14 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         mySwitch = (Switch) findViewById(R.id.mySwitch);
-        rbr = (RadioButton) findViewById(R.id.rbR);
-        rby = (RadioButton) findViewById(R.id.rbY);
-        rbg = (RadioButton) findViewById(R.id.rbG);
-        rbb = (RadioButton) findViewById(R.id.rbB);
         button = (Button) findViewById(R.id.button);
+        layout=(LinearLayout)findViewById(R.id.layout);
+        rg=(RadioGroup)findViewById(R.id.rg);
+
+        rbR=(RadioButton)findViewById(R.id.rbR);
+        rbY=(RadioButton)findViewById(R.id.rbY);
+        rbG=(RadioButton)findViewById(R.id.rbG);
+        rbB=(RadioButton)findViewById(R.id.rbB);
 
         switchState = mySwitch.isChecked();
     }
@@ -34,5 +44,33 @@ public class MainActivity extends AppCompatActivity
     public void switchClicked(View view)
     {
         switchState = mySwitch.isChecked();
+    }
+
+    public void radioButtonClicked(View view)
+    {
+        if(!switchState)
+        {
+            checkRadioButton();
+        }
+    }
+
+    public void checkRadioButton()
+    {
+        if (rbR.isChecked())
+        {
+            layout.setBackgroundColor(Color.RED);
+        }
+        else if (rbY.isChecked())
+        {
+            layout.setBackgroundColor(Color.YELLOW);
+        }
+        else if (rbG.isChecked())
+        {
+            layout.setBackgroundColor(Color.GREEN);
+        }
+        else if (rbB.isChecked())
+        {
+            layout.setBackgroundColor(Color.BLUE);
+        }
     }
 }
